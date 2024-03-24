1e36740cc774205f071bf22f5a8c5fd2bdba7eda
claudioace <claudioace@gmail.com>
2024-03-24 16:29:56 -0300

    final v1.1: ahora home es la pagina inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Home from "./Routes/Home";
 
 
 function App() {
   return (
       <div className="App">
-          <Navbar/>
-          <Footer/>
+          
+          <Home/>
+          
       </div>
   );
 }
